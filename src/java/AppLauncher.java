52d9c90501cf537e0e4e80ae5a371fcdfcedd295
ARROUDJ-Ilhem <123601007+ARROUDJ-Ilhem@users.noreply.github.com>
2023-06-07 19:36:56 +0100

    Add files via upload

---
+++ src/java/AppLauncher.java
@@ -0,0 +1,7 @@
+package tp.javafx;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
---
+++ src/java/Main.java
@@ -0,0 +1,25 @@
+package tp.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("My Desktop Planner");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
